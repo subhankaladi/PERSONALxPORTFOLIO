29e6d1812de7f75fd52818ca3b2d9dd13d1fecb0
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-04-09 16:36:19 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,27 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        'move-right': {
+          '0%, 100%': { transform: 'translateX(-100%)' },
+          '50%': { transform: 'translateX(100%)' }
+        },
+        'move-left': {
+          '0%, 100%': { transform: 'translateX(100%)' },
+          '50%': { transform: 'translateX(-100%)' }
+        },
+        'float': {
+          '0%, 100%': { transform: 'translateY(0) translateX(0)' },
+          '25%': { transform: 'translateY(-10px) translateX(10px)' },
+          '50%': { transform: 'translateY(0) translateX(20px)' },
+          '75%': { transform: 'translateY(10px) translateX(10px)' }
+        }
+      },
+      animation: {
+        'move-right': 'move-right 5s ease-in-out infinite',
+        'move-left': 'move-left 5s ease-in-out infinite',
+        'float': 'float 10s ease-in-out infinite'
+      }
     },
   },
   plugins: [],
