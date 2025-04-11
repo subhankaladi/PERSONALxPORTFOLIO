8a026d8d42ce5a0f4f7155a33bb02384acfc9e82
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-04-11 14:58:43 +0500

    update

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -63,7 +63,7 @@ const Contact = () => {
       } else {
         setSubmitStatus('error');
       }
-    } catch (error) {
+    } catch{
       setSubmitStatus('error');
     } finally {
       setIsSubmitting(false);
